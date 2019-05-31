c39af7f6da89e46ef59fd7931f05d0cf8f1830b6
YangYang <ss880309@gmail.com>
2019-05-31 12:36:24 +0800

    Create Switch it Up!.java

---
+++ kyu8/Switch it Up!.java
@@ -0,0 +1,7 @@
+public class Kata
+{
+  public static String switchItUp(int number)
+  {
+    return number == 0 ? "Zero" : number == 1 ? "One" : number == 2 ? "Two" : number == 3 ? "Three" : number == 4 ? "Four" : number == 5 ? "Five" : number == 6 ? "Six" : number == 7 ? "Seven" : number == 8 ? "Eight" : "Nine";
+  }
+}
