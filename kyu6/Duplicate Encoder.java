a41592dda8b31ae0de7e40e2ffa0db11107da5b1
YangYang <ss880309@gmail.com>
2019-06-11 15:00:40 +0800

    Update Duplicate Encoder.java

--- kyu6/Duplicate Encoder.java
+++ kyu6/Duplicate Encoder.java
@@ -1,18 +1,18 @@
 import java.util.*;
 public class DuplicateEncoder {
 	static String encode(String word){
-  
-    int[] n = new int[200];
+
+	    int[] n = new int[200];
+
+	    for(int i=0;i<word.length();i++)
+	      n[Character.toLowerCase(word.charAt(i))]++;
+
+	    StringBuilder sb = new StringBuilder("");
+
+	    for(int i=0;i<word.length();i++)
+	      sb.append( (n[Character.toLowerCase(word.charAt(i))]>=2)? ')' : '(' );
+
+	    return sb.toString();
     
-    for(int i=0;i<word.length();i++)
-      n[Character.toLowerCase(word.charAt(i))]++;
-    
-    StringBuilder sb = new StringBuilder("");
-    
-    for(int i=0;i<word.length();i++)
-      sb.append( (n[Character.toLowerCase(word.charAt(i))]>=2)? ')' : '(' );
-    
-    return sb.toString();
-    
-  }
+  	}
 }
